0abf713082daf409c23595a705a17df16068706d
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2018-10-01 16:02:48 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
 if array.count == 1
-  array.join(",")
+  array.join
 elsif array.count == 2
   array.join(" and ")
 elsif array.count >= 3
-  array.to_sentence
+  array.to_s
 end
 end
